3df3ad4a2037982737f493219a96f11ead2298e8
Rodgers Gitau <39173463+rodgersgitau@users.noreply.github.com>
2023-06-18 10:12:49 +0000

    feat: todo

---
+++ src/lib/components/todo/TodoSection.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import TodoInput from './TodoInput.svelte';
+
+	import { todos } from '../../store/TodoStore';
+</script>
+
+<section class="mt-8 space-y-4">
+	{#each $todos as todo (todo.id)}
+		<TodoInput {todo} />
+	{/each}
+</section>
